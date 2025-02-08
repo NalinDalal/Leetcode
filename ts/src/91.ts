c72c06eb6e7f8830e1655643179c64c506550cb6
Nalin <nalindalal2004@gmail.com>
2025-02-08 10:24:51 +0530

    leetcode 75 Sort Color

---
+++ ts/src/91.ts
@@ -0,0 +1,6 @@
+//75. Sort Colors
+function sortColors(nums: number[]): void {
+  nums.sort();
+}
+//sub: https://leetcode.com/problems/sort-colors/submissions/1535365908/
+//sol: https://leetcode.com/problems/sort-colors/solutions/6391599/75-sort-colors-by-nalindalal2004-rmyu/
